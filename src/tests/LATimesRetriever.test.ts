2c538d0f20773f02a41190efba2dcb7080bf6035
pwood310 <pwood310@gmail.com>
2020-09-19 16:30:29 -0700

    cleanup inconsistent filename capitalization

--- src/tests/LATimesRetriever.test.ts
+++ src/tests/LATimesRetriever.test.ts
@@ -1,17 +1,17 @@
 import React from 'react';
 //import { render } from '@testing-library/react';
-import LaTimesRetriever from '../lib/LaTimesRetriever';
+import LATimesRetriever from '../lib/LATimesRetriever';
 
 describe("Retriever parsing", () => {
    test('retrieves the data I want', async (done) => {
-     const retriever = new LaTimesRetriever("latimes-county-totals.csv");
+     const retriever = new LATimesRetriever("latimes-county-totals.csv");
      const result = await retriever.retrieve();
      expect("foo").toBe('foo')
      done()
    });
 
   test('transforms csv to array of objects', async (done) => {
-    const retriever = new LaTimesRetriever("something");
+    const retriever = new LATimesRetriever("something");
     const input = `date,county,fips,confirmed_cases,deaths,new_confirmed_cases,new_deaths
 2020-09-18,Alameda,001,20364,370,202,5
 2020-09-18,Los Angeles,037,258587,6330,1244,6
